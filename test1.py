4b5bbc0f6bc30ff487eb0463b2873febab6246d4
rahul shinde <rshinde@mica.edu>
2015-12-30 13:52:26 -0700

    finishing first iteration

--- test1.py
+++ test1.py
@@ -2,17 +2,24 @@
 from PIL import Image
 import sys
 
-im = Image.open("img/1.jpg")
+im = Image.open("img/1.tif")
 
 pixdata = im.load()
 
 
-
 for y in xrange(im.size[1]):
     for x in xrange(im.size[0]):
-        if 252 < pixdata[x, y][0] or 252 < pixdata[x, y][1] or 252 < pixdata[x, y][2]:
+        if 200 < pixdata[x, y][0] or 200 < pixdata[x, y][1] or 200 < pixdata[x, y][2]:
             pixdata[x, y] = (0, 0, 0)
         else:
-            pixdata[x, y] = (0, 0, 0)
+            pixdata[x, y] = (255, 255, 255)
 
-im.show()
+for y in xrange(1, im.size[1]-1):
+    for x in xrange(1, im.size[0]-1):
+        if pixdata[x, y] == (0,0,0) and (pixdata[x + 1, y] == (255, 255, 255) or pixdata[x + 1, y + 1] == (255, 255, 255)):
+            pixdata[x, y] = (0, 0, 0)
+        else:
+            pixdata[x, y] = (255, 255, 255)
+
+im.convert('RGB')
+im.save('img/out.jpg', "JPEG")
