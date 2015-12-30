d8369495ea21836fa614f0d2b5b4901d53517052
rahul shinde <rshinde@mica.edu>
2015-12-30 16:29:02 -0700

    changing img parameters

--- test1.py
+++ test1.py
@@ -2,17 +2,22 @@
 from PIL import Image
 import sys
 
-im = Image.open("img/1.tif")
+imageFileName = raw_input("enter the name of the image file: ")
+
+im = Image.open("img/" + imageFileName)
 
 pixdata = im.load()
 
 
 for y in xrange(im.size[1]):
     for x in xrange(im.size[0]):
-        if 200 < pixdata[x, y][0] or 200 < pixdata[x, y][1] or 200 < pixdata[x, y][2]:
+        if 200 < pixdata[x, y][0] or 100 < pixdata[x, y][1] or 50 < pixdata[x, y][2]:
             pixdata[x, y] = (0, 0, 0)
         else:
             pixdata[x, y] = (255, 255, 255)
+
+im.convert('RGB')
+im.save('img/out1.1.jpg', "JPEG")
 
 for y in xrange(1, im.size[1]-1):
     for x in xrange(1, im.size[0]-1):
@@ -22,4 +27,4 @@ for y in xrange(1, im.size[1]-1):
             pixdata[x, y] = (255, 255, 255)
 
 im.convert('RGB')
-im.save('img/out.jpg', "JPEG")
+im.save('img/out1.2.jpg', "JPEG")
