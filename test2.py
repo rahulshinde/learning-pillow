c8b9fe42739d6a3a9b01f142b8db3dc51071b0f7
rahul shinde <rshinde@mica.edu>
2015-12-30 18:12:45 -0700

    finishing for loop for batch editing files

---
+++ test2.py
@@ -0,0 +1,34 @@
+
+from PIL import Image
+import sys
+import glob
+
+files = glob.glob("img/*.jpg")
+count = 0
+
+for i in files:
+    im = Image.open(i)
+    pixdata = im.load()
+
+    count += 1
+    strCount = str(count)
+
+    for y in xrange(im.size[1]):
+        for x in xrange(im.size[0]):
+            if 150 < pixdata[x, y][0] or 150 < pixdata[x, y][1] or 150 < pixdata[x, y][2]:
+                pixdata[x, y] = (0, 0, 0)
+            else:
+                pixdata[x, y] = (255, 255, 255)
+
+    im.convert('RGB')
+    im.save('out/test2/' + strCount + '.1.jpg', "JPEG")
+
+    for y in xrange(1, im.size[1]-1):
+        for x in xrange(1, im.size[0]-1):
+            if pixdata[x, y] == (0,0,0) and (pixdata[x + 1, y] == (255, 255, 255) or pixdata[x + 1, y + 1] == (255, 255, 255) or pixdata[x, y + 1] == (255, 255, 255) or pixdata[x - 1, y] == (255, 255, 255) or pixdata[x - 1, y - 1] == (255, 255, 255) or pixdata[x, y - 1] == (255, 255, 255)):
+                pixdata[x, y] = (0, 0, 0)
+            else:
+                pixdata[x, y] = (255, 255, 255)
+
+    im.convert('RGB')
+    im.save('out/test2/' + strCount + '.2.jpg', "JPEG")
