8954533d19c41c337912bb2406717e98f36a380f
rahul shinde <rshinde@mica.edu>
2015-12-30 17:50:26 -0700

    new pixel data

--- test1.py
+++ test1.py
@@ -11,7 +11,7 @@ pixdata = im.load()
 
 for y in xrange(im.size[1]):
     for x in xrange(im.size[0]):
-        if 200 < pixdata[x, y][0] or 100 < pixdata[x, y][1] or 50 < pixdata[x, y][2]:
+        if 150 < pixdata[x, y][0] or 150 < pixdata[x, y][1] or 150 < pixdata[x, y][2]:
             pixdata[x, y] = (0, 0, 0)
         else:
             pixdata[x, y] = (255, 255, 255)
@@ -21,7 +21,7 @@ im.save('img/out1.1.jpg', "JPEG")
 
 for y in xrange(1, im.size[1]-1):
     for x in xrange(1, im.size[0]-1):
-        if pixdata[x, y] == (0,0,0) and (pixdata[x + 1, y] == (255, 255, 255) or pixdata[x + 1, y + 1] == (255, 255, 255)):
+        if pixdata[x, y] == (0,0,0) and (pixdata[x + 1, y] == (255, 255, 255) or pixdata[x + 1, y + 1] == (255, 255, 255) or pixdata[x, y + 1] == (255, 255, 255) or pixdata[x - 1, y] == (255, 255, 255) or pixdata[x - 1, y - 1] == (255, 255, 255) or pixdata[x, y - 1] == (255, 255, 255)):
             pixdata[x, y] = (0, 0, 0)
         else:
             pixdata[x, y] = (255, 255, 255)
